7dde9d642ac46333051bda2481b37f4dfd571b7b
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-11-03 09:34:11 +0700

    bai 3 done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import {StyleSheet} from "react-native";
 import {HelloWave} from "@/components/hello-wave";
 import {ThemedText} from "@/components/themed-text";
 import {ThemedView} from "@/components/themed-view";
-import styles from "./styles";
+import styles from "./style";
 
 export default function HomeScreen() {
   return (
--- app/(tabs)/style/index.tsx
+++ app/(tabs)/style/index.tsx
@@ -5,17 +5,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+    marginBlock: "auto",
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+    justifyContent: "center",
   },
 });
 
